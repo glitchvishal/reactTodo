fdc8c0bb6ea1440d434569b7f230e58ca0619e49
vishal singh <vishurajputmgr@gmail.com>
2022-05-03 15:37:18 +0530

    todo

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,7 +1,16 @@
 import React, {useState, useEffect} from 'react'
 import list from '../list'
+import Alert from '../alert'
 
-function Todo() {
+const Todo = () => {
+  
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show:false, message:"", type:""});
+
+
   return (
     <div>Todo</div>
   )
