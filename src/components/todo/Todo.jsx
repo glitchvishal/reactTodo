65302115b54c9c1542824a8e7213fe7bdc2683f0
vishal singh <vishurajputmgr@gmail.com>
2022-05-09 09:29:49 +0530

    handleSubmit

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -26,15 +26,17 @@ const Todo = () => {
       setName("");
       setEditId(null);
       setEdit(false)
-      showAlert(true,"success", "value change");
+      showAlert(true,"Success", "Value Change");
     }else{
-      showAlert(true,"success", "list update");
+      showAlert(true,"Success", "List Update");
       const newItem ={id:new Date().getTime().toString(), title:name}
-      setList([...item, newItem])
+      setList([...list, newItem])
       setName("");
     }
   };
-  const showAlert = () => {};
+  const showAlert = (show = false,type="",message="") => {
+    setAlert({show:show, type:type, message:message})
+  };
   const removeItems = () => {};
   const editItems = () => {};
   const clearList = () => {};
@@ -44,16 +46,16 @@ const Todo = () => {
   return (
     <>
       <div>
-        <from onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
           <h3>Todo List</h3>
           <div>
-            <input type="text" className="form-control" placeholder="write your list" onChange={(e) => setName(e.target.value)} value={name}/>
+            <input type="text" className="form-control" placeholder="Add Your List" onChange={(e) => setName(e.target.value)} value={name}/>
             <button type="button" className="btn btn-success">
               {edit ? "Edit" : "submit"}
             </button>
           </div>
-        </from>
+        </form>
         {list.length > 0 && (
           <div>
             <List item={list} removeItems={removeItems} editItems={editItems} />
